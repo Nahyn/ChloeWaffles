fe99a14a5e2ab122599b401f86e48afb916873b5
Nahyn <nahyn.oklauq@gmail.com>
2026-03-28 22:52:56 +0100

    First step Dialogic

---
+++ scenes/test_dialog.gd
@@ -0,0 +1,30 @@
+extends Control
+class_name DialogScene
+
+@export var dialogic_timeline :DialogicTimeline;
+@export var chloe_character :DialogicCharacter;
+
+func _ready() -> void:
+	Dialogic.Text.about_to_show_text.connect(_on_about_to_show_text)
+	Dialogic.Portraits.character_joined.connect(_on_character_joined)
+	Dialogic.Portraits.character_portrait_changed.connect(_on_character_portrait_changed)
+	start_timeline.call_deferred();
+
+func start_timeline() -> void:
+	Dialogic.start(dialogic_timeline);
+
+func _on_about_to_show_text( _info :Dictionary ) -> void:
+	var character_talking = _info["character"];
+	prints("_on_about_to_show_text")
+	prints("_info", _info)
+	prints("character_talking", character_talking)
+
+func _on_character_portrait_changed( _info :Dictionary ) -> void:
+	prints("_on_character_portrait_changed")
+	prints("_info", _info)
+
+func _on_character_joined( _info :Dictionary ) -> void:
+	prints("_on_character_joined")
+	if _info["character"] == chloe_character:
+		var node = _info["node"] as Node2D;
+		node.z_index = 100;
