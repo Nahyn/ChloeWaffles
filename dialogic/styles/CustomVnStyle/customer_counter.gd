5174dd293fc582f1706abfd9b23cf8aa6738d92c
Nahyn <nahyn.oklauq@gmail.com>
2026-04-01 06:54:37 +0200

    Update Dialogic

--- dialogic/styles/CustomVnStyle/customer_counter.gd
+++ dialogic/styles/CustomVnStyle/customer_counter.gd
@@ -1,2 +1,12 @@
 @tool
 extends DialogicLayoutLayer
+
+const CHLOE_DIALOGIC_CHARACTER :DialogicCharacter = preload("uid://dfjn53vas77fa")
+
+func _ready() -> void:
+	Dialogic.Portraits.character_joined.connect(_on_character_joined)
+
+func _on_character_joined( _info :Dictionary ) -> void:
+	if _info["character"] == CHLOE_DIALOGIC_CHARACTER:
+		var node = _info["node"] as Node2D;
+		node.z_index = 100;
