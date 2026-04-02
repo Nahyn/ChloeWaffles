c8e47e5a2e709b7288593bc51f594e6a9c048b2c
Nahyn <nahyn.oklauq@gmail.com>
2026-04-03 01:32:36 +0200

    Update / Ajout des ressources suites à l'ajout des assets

--- assets/resources/ingredients/ingredient_resource.gd
+++ assets/resources/ingredients/ingredient_resource.gd
@@ -5,5 +5,17 @@ class_name IngredientResource
 @export var name := "[DEFAULT INGREDIENT NAME]";
 
 @export var sprite_texture :Texture2D;
-@export var icon_texture :Texture2D;
+@export var selected_texture :Texture2D;
 @export var stockpile_texture :Texture2D;
+
+func get_selected_texture() -> Texture2D:
+	if selected_texture != null:
+		return selected_texture
+	
+	return get_stockpile_texture()
+
+func get_stockpile_texture() -> Texture2D:
+	if stockpile_texture != null:
+		return stockpile_texture
+	
+	return sprite_texture
---
+++ assets/resources/ingredients/waffle_ingredient_resource.gd
@@ -0,0 +1,4 @@
+extends IngredientResource
+class_name WaffleIngredientResource
+
+@export var batter_texture :Texture2D
