6d14da2574748e2252f64d68604b63bd7f240119
Nahyn <nahyn.oklauq@gmail.com>
2026-04-06 20:48:15 +0200

    Assets update / adding fonts / adding dialogic files

---
+++ assets/resources/days/day_resource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name DayResource
+
+@export var opening_timeline :DialogicTimeline
+@export var closing_timeline :DialogicTimeline
+
+@export var day_score := 0
+@export var day_client_count := 1
+@export var forced_first_client :CharacterResource
